f21ecfbb5a2b10344991399f8b2f0c455559fdcd
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2023-03-08 02:28:57 +0300

    Create TankerTest.java

---
+++ src/Vessels/TankerTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TankerTest {
+
+    @Test
+    void testLoadingCargoWithIncorrectString() {
+        Tanker tanker = new Tanker("B1", 200);
+        tanker.loadingCargo(100, "11");
+        assertEquals(0, tanker.loadFraction());
+    }
+
+    @Test
+    void testLoadingCargoWithNotEnoughSpace() {
+        Tanker tanker = new Tanker("B1", 200);
+        tanker.loadingCargo(100, "2");
+        tanker.loadingCargo(101, "2");
+        assertEquals(0.5, tanker.loadFraction());
+    }
+
+    @Test
+    void testLoadingCargoWithEnoughSpace() {
+        Tanker tanker = new Tanker("B1", 200);
+        tanker.loadingCargo(100, "2");
+        assertEquals(0.1, tanker.loadFraction());
+    }
+
+    @Test
+    void testLoadFractionWithNoCargo() {
+        Tanker tanker = new Tanker("B1", 200);
+        assertEquals(0, tanker.loadFraction());
+    }
+
+    @Test
+    void testLoadFractionWithFullCompartments() {
+        Tanker tanker = new Tanker("B1", 200);
+        for (int i = 1; i <= 10; i++) {
+            tanker.loadingCargo(20, Integer.toString(i));
+        }
+        assertEquals(1.0, tanker.loadFraction());
+    }
+}
