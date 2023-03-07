617c21f0a70fe99c4d8bce13ed193e9a17a2a00b
Mehmet D <55313337+Nerimb@users.noreply.github.com>
2023-03-08 02:27:35 +0300

    Create RoRoVesselTest.java

---
+++ src/Vessels/RoRoVesselTest.java
@@ -0,0 +1,39 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Vessels.RoRoVessel;
+
+public class RoRoVesselTest {
+    RoRoVessel vessel;
+    
+    @Before
+    public void setUp() {
+        vessel = new RoRoVessel("R1", 100);
+    }
+    
+    @Test
+    public void testLoadingCargoForCar() {
+        vessel.loadingCargo(2, "car");
+        assertEquals(16.0, vessel.loadFraction(), 0.01);
+    }
+    
+    @Test
+    public void testLoadingCargoForTruck() {
+        vessel.loadingCargo(1, "truck");
+        assertEquals(0.3, vessel.loadFraction(), 0.01);
+    }
+    
+    @Test
+    public void testLoadingCargoForIncorrectString() {
+        vessel.loadingCargo(2, "plane");
+        assertEquals(0.0, vessel.loadFraction(), 0.01);
+    }
+    
+    @Test
+    public void testLoadingCargoForExceedingCapacity() {
+        vessel.loadingCargo(15, "car");
+        assertEquals(0.0, vessel.loadFraction(), 0.01);
+    }
+}
